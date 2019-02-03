84934d5776a1c6087f417573c6681fb87f862528
Hezi-D <dddavid1984@gmail.com>
2019-02-03 21:10:20 +0200

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-    //ssd
+  //We create array for the image:
+  //If we define the array as NULL - we get error when we
+  // want to apply MAP function when we define it
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 0aab95c1c79478132d1eb9de29a3dae96781cbf700f059ffeddcaa0effc79476"
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
